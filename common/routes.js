05a4ac960e998a373d909d44d96525f8c893db52
Raindeezy <roviir@gmail.com>
2015-12-09 14:29:07 -0800

    static, jscookie

--- common/routes.js
+++ common/routes.js
@@ -3,15 +3,14 @@ import React from 'react';
 import CreatePlaylist from './containers/CreatePlaylist';
 import App from './containers/App';
 import WelcomePage from './containers/WelcomePage';
-import Cookies from 'js-cookie';
 
-function requireAuth(nextState, replaceState) {
-  return
-}
+// function requireAuth(nextState, replaceState) {
+//   return
+// }
 
 export default (
   <Route name='app' path='/' component={App} >
     <IndexRoute component={WelcomePage} />
-    <Route name='playlist' path='/playlist' component={CreatePlaylist} onEnter={requireAuth}/>
+    <Route name='playlist' path='/playlist' component={CreatePlaylist} />
   </Route>
 );
