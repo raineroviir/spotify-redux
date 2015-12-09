234d7bc0d965d328f16561bc120830811bd484c3
Raindeezy <roviir@gmail.com>
2015-12-08 18:43:55 -0800

    viewport

--- productionServer.js
+++ productionServer.js
@@ -95,6 +95,7 @@ function renderFullPage(html, initialState) {
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" />
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css" />
         <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0" />
         <title>spotify3x</title>
       </head>
       <body>
